c1fc3234d11c618c9229e458ca8b06f5719c8db0
juan-aga <juan-aga@student.42malaga.com>
2023-02-28 10:08:12 +0100

    ADD: ft_linux.c
    Now works on limux too.

--- src/ft_linux.c
+++ src/ft_linux.c
@@ -6,7 +6,27 @@
 /*   By: juan-aga <juan_aga@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 19:49:24 by juan-aga          #+#    #+#             */
-/*   Updated: 2023/02/27 14:39:44 by juan-aga         ###   ########.fr       */
+/*   Updated: 2023/02/28 09:50:44 by juan-aga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_memory_leaks.h"
+
+extern void	*__libc_malloc(size_t size);
+extern void *__libc_calloc(size_t size, size_t t);
+extern void __libc_free(void *to_free);
+
+void	*ft_real_malloc(size_t size)
+{
+	return (__libc_malloc(size));
+}
+
+void	*ft_real_calloc(size_t size)
+{
+	return (__libc_calloc(size, 1));
+}
+
+void	ft_real_free(void *p)
+{
+	__libc_free(p);
+}
